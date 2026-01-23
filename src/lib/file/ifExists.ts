5237cd7986a95b43dbc98cb1680de4efa1c244b5
Nokitomo <aronnegiuseppe@gmail.com>
2026-01-23 20:59:35 +0100

    chore: avoid shadowed name in ifExists

--- src/lib/file/ifExists.ts
+++ src/lib/file/ifExists.ts
@@ -8,13 +8,13 @@ export const ifExists = async (fileName: string) => {
     const files = await RNFS.readDir(downloadFolder);
 
     // Find a file with the given name (without extension)
-    const file = files.find(file => {
-      const nameWithoutExtension = file.name.split('.').slice(0, -1).join('.');
+    const match = files.find(item => {
+      const nameWithoutExtension = item.name.split('.').slice(0, -1).join('.');
       return nameWithoutExtension === fileName;
     });
 
-    if (file) {
-      return file.path;
+    if (match) {
+      return match.path;
     } else {
       return false;
     }
