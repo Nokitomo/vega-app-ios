46f92900bf742a4398a54af51436af6d2c452d4e
Nokitomo <aronnegiuseppe@gmail.com>
2026-01-25 11:38:17 +0100

    Remove provider debugLog type

--- src/lib/providers/types.ts
+++ src/lib/providers/types.ts
@@ -155,7 +155,6 @@ export type ProviderContext = {
   Crypto: typeof Crypto;
   getBaseUrl: (providerValue: string) => Promise<string>;
   commonHeaders: Record<string, string>;
-  debugLog?: (message: string, data?: unknown) => void;
   cheerio: typeof cheerio;
   extractors: {
     hubcloudExtracter: (link: string, signal: AbortSignal) => Promise<Stream[]>;
