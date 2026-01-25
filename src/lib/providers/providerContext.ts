3569d9a7eed87f4c6eab2ce092502e2351ab27d4
Nokitomo <aronnegiuseppe@gmail.com>
2026-01-25 11:01:34 +0100

    Add axios debug logs for AnimeUnity top

--- src/lib/providers/providerContext.ts
+++ src/lib/providers/providerContext.ts
@@ -29,3 +29,49 @@ export const providerContext: ProviderContext = {
   cheerio,
   extractors,
 };
+
+const shouldLogAnimeunityTop = (url?: string) =>
+  typeof url === 'string' && url.includes('animeunity.so/top-anime');
+
+axios.interceptors.request.use(
+  config => {
+    if (shouldLogAnimeunityTop(config.url)) {
+      console.log('[animeunity][top] request', {
+        url: config.url,
+        params: config.params,
+      });
+    }
+    return config;
+  },
+  error => Promise.reject(error),
+);
+
+axios.interceptors.response.use(
+  response => {
+    if (shouldLogAnimeunityTop(response.config?.url)) {
+      const size =
+        typeof response.data === 'string'
+          ? response.data.length
+          : Array.isArray(response.data?.data)
+            ? response.data.data.length
+            : undefined;
+      console.log('[animeunity][top] response', {
+        url: response.config?.url,
+        status: response.status,
+        size,
+      });
+    }
+    return response;
+  },
+  error => {
+    const url = error?.config?.url;
+    if (shouldLogAnimeunityTop(url)) {
+      console.log('[animeunity][top] response error', {
+        url,
+        status: error?.response?.status,
+        message: error?.message,
+      });
+    }
+    return Promise.reject(error);
+  },
+);
