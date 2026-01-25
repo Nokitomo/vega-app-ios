e749b3766cca602aee3ba71d70fbfcbb805c690c
Nokitomo <aronnegiuseppe@gmail.com>
2026-01-25 11:38:50 +0100

    Remove provider context debug interceptors

--- src/lib/providers/providerContext.ts
+++ src/lib/providers/providerContext.ts
@@ -21,66 +21,11 @@ const extractors = {
   gdFlixExtracter,
 };
 
-const debugLog = (message: string, data?: unknown) => {
-  if (data === undefined) {
-    console.log(message);
-    return;
-  }
-  console.log(message, data);
-};
-
 export const providerContext: ProviderContext = {
   axios,
   getBaseUrl,
   commonHeaders: headers,
   Crypto,
-  debugLog,
   cheerio,
   extractors,
 };
-
-const shouldLogAnimeunityTop = (url?: string) =>
-  typeof url === 'string' && url.includes('animeunity.so/top-anime');
-
-axios.interceptors.request.use(
-  config => {
-    if (shouldLogAnimeunityTop(config.url)) {
-      console.log('[animeunity][top] request', {
-        url: config.url,
-        params: config.params,
-      });
-    }
-    return config;
-  },
-  error => Promise.reject(error),
-);
-
-axios.interceptors.response.use(
-  response => {
-    if (shouldLogAnimeunityTop(response.config?.url)) {
-      const size =
-        typeof response.data === 'string'
-          ? response.data.length
-          : Array.isArray(response.data?.data)
-            ? response.data.data.length
-            : undefined;
-      console.log('[animeunity][top] response', {
-        url: response.config?.url,
-        status: response.status,
-        size,
-      });
-    }
-    return response;
-  },
-  error => {
-    const url = error?.config?.url;
-    if (shouldLogAnimeunityTop(url)) {
-      console.log('[animeunity][top] response error', {
-        url,
-        status: error?.response?.status,
-        message: error?.message,
-      });
-    }
-    return Promise.reject(error);
-  },
-);
