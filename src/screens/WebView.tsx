653919f19dda9a5df28ffb4ee196f2260f741380
Nokitomo <aronnegiuseppe@gmail.com>
2026-01-30 22:29:38 +0100

    feat: localizza webview

--- src/screens/WebView.tsx
+++ src/screens/WebView.tsx
@@ -4,14 +4,16 @@ import {WebView} from 'react-native-webview';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {HomeStackParamList} from '../App';
 import {MaterialIcons} from '@expo/vector-icons';
+import {useTranslation} from 'react-i18next';
 
 type Props = NativeStackScreenProps<HomeStackParamList, 'Webview'>;
 
 const Webview = ({route, navigation}: Props) => {
+  const {t} = useTranslation();
   return (
     <SafeAreaView className="bg-black w-full h-full">
       <View className="bg-black w-full mt-6 h-16 flex flex-row justify-between p-3 items-center">
-        <Text className="text-white text-lg font-bold">Webview</Text>
+        <Text className="text-white text-lg font-bold">{t('Webview')}</Text>
         <View className="flex flex-row items-center gap-5">
           <MaterialIcons
             name="open-in-browser"
