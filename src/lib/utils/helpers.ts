812cd9cd84f61db6a3acc2b0d0600046f6a763fe
Nokitomo <aronnegiuseppe@gmail.com>
2026-02-03 23:50:47 +0100

    Aggiungi helper badge ITA

--- src/lib/utils/helpers.ts
+++ src/lib/utils/helpers.ts
@@ -2,3 +2,6 @@ export const formatName = (name: string): string => {
   // Replace special characters with an underscore
   return name.replaceAll(/[^a-zA-Z0-9]/g, '_');
 };
+
+export const hasItaBadge = (title?: string): boolean =>
+  /\(\s*ita\s*\)/i.test(title || '');
